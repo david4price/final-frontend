7960fcc73a6c6838c4fca10385d19f533a56ffae
David Praise <david.praise2323@gmail.com>
2023-02-16 22:58:34 +0200

    fixed link to git

--- src/Components/Header/MyHeader.js
+++ src/Components/Header/MyHeader.js
@@ -5,7 +5,7 @@ const MyHeader = () => {
     <div>
       <br />
       <p>Checkout My GitHub Repository</p>
-      <a href="https://github.com/david4price/final-dance.git">Backend</a>
+      <a href="https://github.com/david4price/final-dance.git">Backend</a> - 
       <a href="https://github.com/david4price/final-frontend.git">Frontend</a>
     </div>
   );
